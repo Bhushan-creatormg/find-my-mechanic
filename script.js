1d86b0cdd4025684a0632294c49c85d4b8e24e45
Bhushan-creatormg <bhushanbhalerao2205@gmail.com>
2025-03-07 22:38:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,33 @@ function loadMechanics() {
 }
 
 window.onload = loadMechanics;
+function getUserLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    alert("Your location:\nLatitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude);
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
+}
+
+// Add a button to HTML to trigger location tracking
