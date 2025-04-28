ceaa8e9c5d634155f82cc688b8b03b6c1c5fef54
Bhushan-creatormg <bhushanbhalerao2205@gmail.com>
2025-04-28 21:23:17 +0530

    Update branddata.js

--- branddata.js
+++ branddata.js
@@ -1,32 +1,20 @@
-// Data for brand logos
-const brands = {
+const brandData = {
   bike: [
-    { name: 'Royal Enfield', logo: 'images/royal-enfield.png' },
-    { name: 'Honda', logo: 'images/honda.png' },
-    { name: 'Yamaha', logo: 'images/yamaha.png' }
+    { name: "Royal Enfield" },
+    { name: "Honda" },
+    { name: "Bajaj" },
+    { name: "TVS" },
+    { name: "Suzuki" }
   ],
   car: [
-    { name: 'Maruti Suzuki', logo: 'images/maruti.png' },
-    { name: 'Hyundai', logo: 'images/hyundai.png' },
-    { name: 'Toyota', logo: 'images/toyota.png' }
+    { name: "Maruti Suzuki" },
+    { name: "Hyundai" },
+    { name: "Tata Motors" },
+    { name: "Mahindra" },
+    { name: "Toyota" }
   ],
   others: [
-    { name: 'Truck', logo: 'images/truck.png' },
-    { name: 'Rickshaw', logo: 'images/rickshaw.png' }
+    { name: "Rickshaw" },
+    { name: "Truck" }
   ]
 };
-
-// Function to update brand dropdown based on vehicle type
-function updateBrandOptions(vehicleType) {
-  const brandSelect = document.getElementById('brand');
-  brandSelect.innerHTML = '<option value="">Select Brand</option>'; // Clear previous options
-
-  if (brands[vehicleType]) {
-    brands[vehicleType].forEach(brand => {
-      const option = document.createElement('option');
-      option.value = brand.name;
-      option.innerHTML = `<img src="${brand.logo}" alt="${brand.name}" width="20px"> ${brand.name}`;
-      brandSelect.appendChild(option);
-    });
-  }
-}
